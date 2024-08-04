c6058a9b9e93fa7f921ec0934d2ff5ab98c55596
Thiago Silva <contato.dev.thiagosilva@gmail.com>
2024-08-04 15:37:39 -0300

    Routes para o welcomeBackPage foram removidas

--- lib/routes/app_page.dart
+++ lib/routes/app_page.dart
@@ -3,14 +3,12 @@ import '../features/auth/pages/walkthrough/walkthrough_page.dart';
 import '../features/auth/pages/welcome/welcome_page.dart';
 import '../features/auth/pages/privacy_policy/privacy_policy_page.dart';
 import '../features/auth/pages/service_terms/service_terms_page.dart';
-import '../features/auth/welcome_back/page/welcome_back_page.dart';
 
 class AppPages {
   static final page = [
     GetPage(name: "/", page: () => const WalkthroughPage()),
     GetPage(name: "/auth", page: () => const WalkthroughPage()),
     GetPage(name: "/welcome", page: () => const WelcomePage()),
-    GetPage(name: "/welcome_back", page: () => const WelcomeBackPage()),
     GetPage(name: "/privacy_policy", page: () => const PrivacyPolicyPage()),
     GetPage(name: "/service_terms", page: () => const ServiceTermsPage()),
   ];
